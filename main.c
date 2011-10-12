43d70809237ec9d6f80fa0dcf006a7b1c7e38f47
Arunesh Mathur <aruneshmathur1990@gmail.com>
2011-10-13 01:41:02 +0530

    Assignment function added

--- main.c
+++ main.c
@@ -2,10 +2,19 @@
 #include<stdlib.h>
 #include<string.h>
 
-int* assign(int *arr, int size, char type) {
+void cumulate(int *a, int size) {
+	int i;
+	for(i = 1; i < size; i++) {
+		a[i] = a[i] + a[i-1];
+	}
+}
+
+int* assign(char *arr, int size, char type) {
 
  	int hash[26], base, i;
 	memset(hash, 0, sizeof(hash));
+
+	int* new_arr = malloc(sizeof(arr));
 
 	switch(type) {
 		case 'a': base = 97; 
@@ -23,6 +32,14 @@ int* assign(int *arr, int size, char type) {
 	for(i = 0; i < size; i++) {
 		hash[arr[i]-base]++;
 	}
+	
+	cumulate(hash, 26);
+
+	for(i = size - 1; i >= 0; i--) {
+		new_arr[i] = (hash[arr[i]-base]--) % 10;
+	}
+
+	return new_arr;
 }
 		
 
@@ -39,6 +56,10 @@ int* chain_addition(int *arr, int size) {
 }
 
 int main() {
+
+	char array[10] = { 'A', 'N', 'N', 'I', 'E', 'W', 'I', 'T', 'H', 'T'}; 
+	int* res = assign(array, 10, 'A'), i;
+	for(i=0;i<10;i++) printf("%d ", res[i]);
 
 	return 0;
 }
