a0a91dee47e2f0e21277166e52e84b4b389a2e84
Arunesh Mathur <aruneshmathur1990@gmail.com>
2011-10-30 23:11:44 +0530

    ISO style

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ An implementation of the VIC cipher in C
 A course project to be submitted as a part of the Cryptography course at NITK
 Surathkal.
 
-To be updated soon.
+As of now, only the encoder is funtional. A CLI will be added, with *NIX style 
+options. Till then, have a look at main.c.
+
+-Arunesh Mathur
--- main.c
+++ main.c
@@ -5,10 +5,11 @@
 
 int main() {
 
-	int i, j, count = 0;
+	int i, j, count = 0, encode_result_length, first_num, second_num, elecount, rows;
 	int date[6] = {7, 4, 1, 7, 7, 6};
 	int random_ig[5] = {7, 7, 6, 5, 1};
-	int* temp;
+	int *temp, *first_app_res, *assign_res, *header_checker, *encode_result, *transpose_select, *new_header;
+	int **pseudo_random, **transpose_res, **second_num_trans, **final_res, **inter_trans, **message_matrix;
 
 	int* sub_result = modular_subtraction_digit(random_ig, date, 5);
 	char* key_phrase = "IDREAMOFJEANNIEWITHT";
@@ -24,31 +25,32 @@ int main() {
 	free(sub_result);
 	free(chain_res);
 	
-	int* first_app_res = modular_addition_digit(first, append_res, 10);
+	first_app_res = modular_addition_digit(first, append_res, 10);
 	
-	int* assign_res = map_change(second, first_app_res, 10);
+	assign_res = map_change(second, first_app_res, 10);
+	free(first_app_res);
 	
-	int** pseudo_random = malloc(sizeof(int*) * 5);
+	pseudo_random = malloc(sizeof(int*) * 5);
 
 	temp = assign_res;
-	for(i=0;i<5;i++) {
+	for(i = 0; i < 5; i++) {
 		temp=chain_addition(temp,10);
 		pseudo_random[i]=temp;
 	}
-
-	int** trans_res = transpose(assign_res, pseudo_random, 10, 5);
-
-	int* header_checker = assign_int(pseudo_random[4], 0, 10);
-
-	int* encode_result = encode(message, header_checker, strlen(message));
-	int encode_result_length = get_encode_message_length(encode_result);
-
-	int first_num = 8 + pseudo_random[4][8];
-	int second_num = 8 + pseudo_random[4][9];
-
-	int** transpose_res = transpose(assign_res, pseudo_random, 10, 5);
 	
-	int* transpose_select = malloc(sizeof(int) * first_num + second_num);
+	header_checker = assign_int(pseudo_random[4], 0, 10);
+
+	encode_result = encode(message, header_checker, strlen(message));
+
+	encode_result_length = get_encode_message_length(encode_result);
+
+	first_num = 8 + pseudo_random[4][8];
+	second_num = 8 + pseudo_random[4][9];
+
+	transpose_res = transpose(assign_res, pseudo_random, 10, 5);
+	free(temp);
+	
+	transpose_select = malloc(sizeof(int) * first_num + second_num);
 
 	for(i = 0; i < 10; i++) {
 		for(j = 0; j < 5; j++) {
@@ -58,21 +60,23 @@ int main() {
 			if (count == 31) break;
 	}
 
-	int** message_matrix = get_matrix(encode_result, encode_result_length, first_num);
+	message_matrix = get_matrix(encode_result, encode_result_length, first_num);
+	free(encode_result);
 
-	int elecount = (encode_result_length + 5 - (encode_result_length % 5));
-	int rows = elecount / first_num;
+	elecount = (encode_result_length + 5 - (encode_result_length % 5));
+	rows = elecount / first_num;
 	if(encode_result_length % first_num !=  0) rows =rows + 1;
 
-	int** inter_trans = transpose(transpose_select, message_matrix, first_num, rows);
+	inter_trans = transpose(transpose_select, message_matrix, first_num, rows);
 
-	int* new_header = copy(transpose_select, first_num, first_num + second_num);
+	new_header = copy(transpose_select, first_num, first_num + second_num);
 
-	int** second_num_trans = get_matrix_filled(inter_trans, first_num, rows, elecount, new_header, second_num);
+	second_num_trans = get_matrix_filled(inter_trans, first_num, rows, elecount, new_header, second_num);
 
 	rows = elecount / second_num;
 	if(encode_result_length % second_num !=  0) rows =rows + 1;
-	int** final_res = transpose(new_header, second_num_trans, second_num, rows);
+	final_res = transpose(new_header, second_num_trans, second_num, rows);
+	free(new_header);
 	
 	elecount = 1;
 	
